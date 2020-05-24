4da5c0933e9f266b45d70ce7cdb9e4d4e7a45d18
vai1205 <vaibhav.12may95@gmail.com>
2020-05-24 12:07:10 +0530

    ready to deploy

--- config.js
+++ config.js
@@ -4,7 +4,6 @@ export const API = publicRuntimeConfig.PRODUCTION
   ? publicRuntimeConfig.API_PRODUCTION
   : publicRuntimeConfig.API_DEVELOPMENT;
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
-export const FB_APP_ID = publicRuntimeConfig.FB_APP_ID;
 export const DOMAIN = publicRuntimeConfig.PRODUCTION
   ? publicRuntimeConfig.DOMAIN_PRODUCTION
   : publicRuntimeConfig.DOMAIN_DEVELOPMENT
